2017796691273ee3e905ce334209cca580e62d05
Nick Thorpe <nickthorpe71@gmail.com>
2023-10-26 21:43:32 -0500

    add haskell solution

--- 5-string-construct/Haskell/5-string-construct.hs
+++ 5-string-construct/Haskell/5-string-construct.hs
@@ -4,21 +4,29 @@ import Data.Time (getCurrentTime, diffUTCTime)
 
 -- Define functions
 stringConstructing :: String -> String -> Int
-stringConstructing a s = 
+stringConstructing a s = construct 0 0 "" where
+  construct i ops curr
+    | curr == s = ops
+    | otherwise =
+      let (c', i') = findMismatch i curr s
+      in if i' >= length curr
+        then construct i' (ops + 1) (curr ++ a)
+        else construct i' (ops + 1) (take i' c' ++ drop (i' + 1) c')
 
-levelshtein :: String -> String -> Int
-levelshtein s1 s2 = last $ foldl transform [0..length s1] s2
-  where
-    transform sx@(x:xs) c = scanl compute (x + 1) (zip3 s1 xs xs')
-      where
-        compute z (c', x, y) = minimum [y + 1, z + 1, x + if c == c' then 0 else 1]
+findMismatch :: Int -> String -> String -> (String, Int)
+findMismatch i c s
+  | i < length c && i < length s && c !! i == s !! i = findMismatch (i + 1) c s
+  | otherwise = (c, i)
+
 
 main :: IO ()
 main = do
   start <- getCurrentTime
   -- run function
-  let result = oddOrEven 1
-  putStrLn $ "res: " ++ show result
+  putStrLn . show $ stringConstructing "a" "a"
+  putStrLn . show $ stringConstructing "aba" "abbabba"
+  putStrLn . show $ stringConstructing "a" "aaa"
+  putStrLn . show $ stringConstructing "bbaabcbcbc" "bbcccbabcc" 
   end <- getCurrentTime
   print (diffUTCTime end start)
 
