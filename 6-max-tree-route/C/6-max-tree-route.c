0e0125283b4cbfe5f5083c4f353a135bd37ee14c
Nick Thorpe <nickthorpe71@gmail.com>
2023-11-07 21:50:23 -0600

    add C implementation

--- 6-max-tree-route/C/6-max-tree-route.c
+++ 6-max-tree-route/C/6-max-tree-route.c
@@ -1,5 +1,48 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<time.h>
+#include<math.h>
+#include<limits.h>
+
+// Definition of the TreeNode struct
+typedef struct TreeNode {
+  int value;
+  struct TreeNode* left;
+  struct TreeNode* right;
+} TreeNode;
+
+// Function to create a new TreeNode
+TreeNode* newTreeNode(int value, TreeNode* left, TreeNode* right) {
+  TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+  if (!node) {
+    return NULL;
+  }
+  node->value = value;
+  node->left = left;
+  node->right = right;
+  return node;
+}
+
+// Function to calculate the maximum sum from root to any leaf
+int maxSum(TreeNode* root) {
+  if (root == NULL) {
+    return 0;
+  } else if (root->left == NULL && root->right == NULL) {
+    return root->value;
+  } else {
+    int leftMax = root->left ? maxSum(root->left) : INT_MIN;
+    int rightMax = root-> right ? maxSum(root->right) : INT_MIN;
+    return root->value + (leftMax > rightMax ? leftMax : rightMax);
+  }
+}
+
+// Function to free the memory of a tree
+void freeTree(TreeNode* root) {
+  if (root == NULL) return;
+  freeTree(root->left);
+  freeTree(root->right);
+  free(root);
+}
 
 int main() 
 {
@@ -7,7 +50,31 @@ int main()
   double cpu_time_used;
 
   start = clock();
-  //call functions here
+  
+  // Create sample tree
+  TreeNode* sampleTree = newTreeNode(
+    17,
+    newTreeNode(
+      3,
+      newTreeNode(2, NULL, NULL),
+      NULL
+    ),
+    newTreeNode(
+      -10,
+      newTreeNode(16, NULL, NULL),
+      newTreeNode(
+        1,
+        newTreeNode(13, NULL, NULL),
+        NULL
+      )
+    )
+  );
+
+  // Calculate the max sum of any one tree path
+  printf("Max Sum: %d\n", maxSum(sampleTree));
+
+  freeTree(sampleTree);
+
   end = clock();
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
   printf("Time elapsed: %f\n", cpu_time_used);
--- 6-max-tree-route/summary.md
+++ 6-max-tree-route/summary.md
@@ -5,3 +5,8 @@ This [kata](https://www.codewars.com/kata/57e5279b7cf1aea5cf000359) was a nice c
 
 **JS** 
 Once again I started with JS. It seems like I either need to get better with one of the languages to feel confortable using those while I think through the problem or just accept that JS will remain the first language. 
+
+**C**
+Was able to get some more practice and a better understanding in allocating and freeing memory with this exercise. I would like to start the next problem with C as it seems to help me visualize an image of how the data is passed and structured.
+
+
