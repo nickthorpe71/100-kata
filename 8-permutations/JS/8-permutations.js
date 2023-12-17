f28a2803cdc023fcbddb8502a43d2bbdd8ddc620
Nick Thorpe <nickthorpe@Nicks-MacBook-Pro.local>
2023-12-17 13:35:25 -0600

    add JS solution

--- 8-permutations/JS/8-permutations.js
+++ 8-permutations/JS/8-permutations.js
@@ -1,6 +1,30 @@
+function permutations(str) {
+  if (str.length < 2) return [str];
+
+  let perms = [];
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (str.indexOf(char) != i) continue;
+
+    const remainingChars = str.slice(0, i) + str.slice(i + 1, str.length);
+
+    for (const permutation of permutations(remainingChars)) {
+      perms.push(char + permutation);
+    }
+  }
+  
+  return perms;
+}
+
+
 function main() {
   console.time('Execution Time');
-  console.log("Hello World!");
+  
+  console.log(permutations("a"));
+  console.log(permutations("aabb"));
+  console.log(permutations("ab"));
+
   console.timeEnd('Execution Time');
 }
 
