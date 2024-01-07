70bd52c7b966c945e44020d34673040e1a9e2a7e
Nick Thorpe <nickthorpe71@gmail.com>
2024-01-07 15:17:07 -0600

    add JS solution for kata 11

--- 11-string-reverse/JS/11-string-reverse.js
+++ 11-string-reverse/JS/11-string-reverse.js
@@ -1,6 +1,12 @@
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
+
 function main() {
   console.time('Execution Time');
-  console.log("Hello World!");
+  console.log(spinWords("Test Testing lamamama 1 1 1 asd  dfs   "));
   console.timeEnd('Execution Time');
 }
 
