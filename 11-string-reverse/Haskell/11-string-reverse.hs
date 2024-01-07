441fe88d51a95c47395f51adbcde6154aa1b1126
Nick Thorpe <nickthorpe71@gmail.com>
2024-01-07 15:28:55 -0600

    add haskell solutionb  forr kata 11

--- 11-string-reverse/Haskell/11-string-reverse.hs
+++ 11-string-reverse/Haskell/11-string-reverse.hs
@@ -1,17 +1,18 @@
-module Main where
+import Data.List (intercalate)
+import System.CPUTime (getCPUTime)
+import Text.Printf (printf)
 
-import Data.Time (getCurrentTime, diffUTCTime)
-
--- Define functions
-oddOrEven :: Int -> String
-oddOrEven n = if odd n then "Odd" else "Even"
+spinWords :: String -> String
+spinWords = intercalate " " . map spin . words
+  where
+    spin word
+      | length word > 4 = reverse word
+      | otherwise       = word
 
 main :: IO ()
 main = do
-  start <- getCurrentTime
-  -- run function
-  let result = oddOrEven 1
-  putStrLn $ "res: " ++ show result
-  end <- getCurrentTime
-  print (diffUTCTime end start)
-
+  startTime <- getCPUTime
+  putStrLn $ spinWords "Test Testing lamamama 1 1 1 asd dfs   "
+  endTime <- getCPUTime
+  let diff = fromIntegral (endTime - startTime) / (10^12)  -- converting picoseconds to seconds
+  printf "Execution Time: %0.3f sec\n" (diff :: Double)
