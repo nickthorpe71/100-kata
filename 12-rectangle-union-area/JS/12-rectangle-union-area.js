9a0ac2065bb93e73f15d83f00cf983fe0243b182
Nick Thorpe <nickthorpe71@gmail.com>
2024-01-10 07:58:07 -0600

    add completed JS solution for kata 12

--- 12-rectangle-union-area/JS/12-rectangle-union-area.js
+++ 12-rectangle-union-area/JS/12-rectangle-union-area.js
@@ -4,62 +4,42 @@
  * @returns {number}
  */
 function calculate(recs) {
-    if (recs.length === 0) return 0;
+    let steps = new Set();
+    recs.forEach((e) => {
+        steps.add(e[0]) && steps.add(e[2]);
+    });
 
-    // [x] get area of all rectangles
-    // [x] find overlapping rectangles
-    // [ ] find points of overlaps
-    // [x] find area of overlaps
-    // [x] subtract overlaps area from total area
+    recs = recs.sort((a, b) => a[0] - b[0]);
 
-    const totalArea = recs.reduce((total, rec) => {
-        return total + calculateArea(rec);
-    }, 0);
-    const overlaps = findOverlaps(recs);
-    const overlappingPoints = calculateOverlappingPoints(overlaps);
-    const overlappingArea = overlappingPoints.map((rec) => calculateArea(rec));
+    let square = (offset = 0);
+    let activeRecs = [];
+    [...steps]
+        .sort((a, b) => a - b)
+        .reduce((prevStep, step) => {
+            let count = 0;
+            activeRecs
+                .sort((a, b) => a[1] - b[1])
+                .reduce((prev, cur) => {
+                    if (prev < cur[3]) {
+                        count += cur[3] - Math.max(cur[1], prev);
+                        return cur[3];
+                    }
+                    return prev;
+                }, 0);
+            square += count * (step - prevStep);
 
-    // return totalArea - overlappingArea
+            activeRecs = activeRecs.filter((r) => r[2] > step);
 
-    return { totalArea, overlaps };
-}
-
-function calculateArea(rec) {
-    // each rectangle is represented as: [x0, y0, x1, y1]
-    // (x0, y0) - coordinates of the bottom left corner
-    // (x1, y1) - coordinates of the top right corner
-    // xi, yi - positive integers or zeroes (0, 1, 2, 3, 4..)
-    // sides of rectangles are parallel to coordinate axes
-    return Math.abs(rec[2] - rec[0]) * Math.abs(rec[3] - rec[1]);
-}
-
-function findOverlaps(recs) {
-    let overlaps = new Set();
-
-    for (let i = 0; i < recs.length - 1; i++) {
-        const rec1 = recs[i];
-        const rec2 = recs[i + 1];
-
-        // Rectangles overlap if one rectangle's bottom left corner is to the left
-        // and below the top right corner of the other rectangle and vice versa.
-
-        const overlapping =
-            rec1[0] <= rec2[2] &&
-            rec1[1] <= rec2[3] &&
-            rec2[0] <= rec1[2] &&
-            rec2[1] <= rec1[3];
-
-        if (overlapping) {
-            overlaps.add(rec1);
-            overlaps.add(rec2);
-        }
-    }
-
-    return overlaps;
-}
-
-function calculateOverlappingPoints(overlaps) {
-    return [];
+            for (
+                let i = offset;
+                i in recs && recs[i][0] === step;
+                offset = ++i
+            ) {
+                activeRecs.push(recs[i]);
+            }
+            return step;
+        }, 0);
+    return square;
 }
 
 function main() {
