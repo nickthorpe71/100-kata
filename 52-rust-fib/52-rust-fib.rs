17c0895456a7c1c7fa330372a7cfc0d86cd9af69
Nick Thorpe <nickthorpe71@gmail.com>
2025-01-20 19:51:53 -0800

    add kata 52 rust recursion practice

---
+++ 52-rust-fib/52-rust-fib.rs
@@ -0,0 +1,23 @@
+fn fib(n: i64) -> i64 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+
+    fib(n - 1) + fib(n - 2)
+}
+
+fn main() {
+    println!("{}", fib(0)); // 0
+    println!("{}", fib(1)); // 1
+    println!("{}", fib(2)); // 1
+    println!("{}", fib(3)); // 2
+    println!("{}", fib(4)); // 3
+    println!("{}", fib(5)); // 5
+    println!("{}", fib(6)); // 8
+    println!("{}", fib(7)); // 13
+    println!("{}", fib(8)); // 21
+    println!("{}", fib(9)); // 34
+    println!("{}", fib(10)); // 55
+    println!("{}", fib(11)); // 89
+    println!("{}", fib(12)); // 144
+}
