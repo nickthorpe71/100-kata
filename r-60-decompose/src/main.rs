3c72a2f879eac79367afd5073e164170302f7ef5
Nick Thorpe <nickthorpe71@gmail.com>
2025-02-05 06:30:19 -0800

    add alt solution to kata 60

--- r-60-decompose/src/main.rs
+++ r-60-decompose/src/main.rs
@@ -20,8 +20,24 @@ fn decompose(n: i64) -> Option<Vec<i64>> {
     helper(n * n, n)
 }
 
+fn decompose_b(n: i64) -> Option<Vec<i64>> {
+    partition(n * n, n)
+}
+
+fn partition(sqrt: i64, n: i64) -> Option<Vec<i64>> {
+    if sqrt < 0 { return None; }
+    if sqrt == 0 { return Some(Vec::new()); }
+
+    for i in (0..n).rev() {
+        let part = partition(sqrt - i * i, i);
+        if part != None { return Some([part.unwrap(), vec![i]].concat()); }
+    }
+
+    None
+}
+
 fn main() {
     println!("Decompose 11: {:?}", decompose(11)); // Expected: Some([1, 2, 4, 10])
-    println!("Decompose 50: {:?}", decompose(50)); // Expected: Some([1, 3, 5, 8, 49])
-    println!("Decompose 4: {:?}", decompose(4));   // Expected: None (since no valid decomposition exists)
+    println!("Decompose 50: {:?}", decompose_b(50)); // Expected: Some([1, 3, 5, 8, 49])
+    println!("Decompose 4: {:?}", decompose_b(4));   // Expected: None (since no valid decomposition exists)
 }
